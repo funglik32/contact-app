5184f1ce8509cc6fa926198fcede4c0a763dab15
funglik32 <fahmata32@gmail.com>
2024-12-17 15:23:52 +0700

    initial commit

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+ return [
+   {
+     id: 1,
+     name: 'Dimas Saputra',
+     tag: 'dimasmds',
+     imageUrl: '/image/dimasmds.jpeg',
+   },
+   {
+     id: 2,
+     name: 'Arif Faizin',
+     tag: 'arifaizin',
+     imageUrl: '/image/arifaizin.jpeg',
+   },
+   {
+     id: 3,
+     name: 'Rahmat Fajri',
+     tag: 'rfajri27',
+     imageUrl: '/image/rfajri27.jpeg',
+   },
+ ];
+}
+ 
+export { getData };
